0e77399ae114a50293cf5f74695e3f9e1db5fd6b
조경진 <kj_cho@idealidea.co.kr>
2020-06-20 19:22:42 +0900

    로그인 동작 흉내 코드 작성

--- app/src/main/java/kr/co/namu/login_20200620/MainActivity.kt
+++ app/src/main/java/kr/co/namu/login_20200620/MainActivity.kt
@@ -2,10 +2,30 @@ package kr.co.namu.login_20200620
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+//            입력한 이메일 / 비번 가져오기
+            val email = emailEdt.text.toString()
+            val password = pwEdt.text.toString()
+
+//            이메일 / 비번 동시에 검사
+            if (email == "admin" && password == "qwer") {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
     }
 }
